27939e07f2107e052f9eefde30251f4a6493f8bf
Zad3 (ノωヽ) <51686336+ZadeTheExplorer@users.noreply.github.com>
2021-03-12 10:41:09 +0700

    add Neuron Net Notes

---
+++ src/pages/NeuronNetNote.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      <embed src=".\neuronNetNotes\Neuron_Net_Beginner_Guide.txt">
+  </div>
+</template>
+<script>
+import { Tabs, TabPane } from '@/components';
+
+export default {
+  name: 'NeuronNetNote',
+  bodyClass: 'neuron-net-note-page',
+  components: {
+    
+  },
+  data: function() {
+return {
+    name: "note"
+  };
+}
+};
+</script>
+<style></style>
